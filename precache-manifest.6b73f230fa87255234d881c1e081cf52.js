9e580f61c7c6e15d6a59719e4af5151e7566f380
NicolasAudepin <42012602+NicolasAudepin@users.noreply.github.com>
2021-02-25 16:55:19 +0100

    Updates

---
+++ precache-manifest.6b73f230fa87255234d881c1e081cf52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd76a7c9d4fe602da97ab09696159d1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2671ba51739b6f05933c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "232aadf922433ff46806",
+    "url": "/static/js/2.9ede02d8.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/static/js/2.9ede02d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2671ba51739b6f05933c",
+    "url": "/static/js/main.0b3c8a6b.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
