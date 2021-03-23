fc672d4b13d8da4a198e4522ec9094185e57c3c4
NicolasAudepin <42012602+NicolasAudepin@users.noreply.github.com>
2021-03-23 01:48:54 +0100

    I need to set the midi

--- src/DataVis.jsx
+++ src/DataVis.jsx
@@ -19,14 +19,28 @@ class DataVis extends Component {
             name: classname,
             count: detections.filter(item => item.class === classname).length
         }));
+
+        const tv = detections.filter(item => item.class === "tv").length
+        const laptop = detections.filter(item => item.class === "laptop").length
+        const phone = detections.filter(item => item.class === "cell phone").length
+        const person = detections.filter(item => item.class === "person").length
+
+        
         
         return (
             <div>
                 <h2>DATAVIS</h2>
 
                 <div>
+                    <h3>Detected Stuff</h3>
                     {counts.map((detections,index) => <p>{detections.name} : {detections.count}</p>)}
                 </div>
+                <div>
+                    <h3>Cool Values</h3>
+                    Screen Per Personne :{(tv + laptop + phone)/ (Math.max(person,1) )}
+
+                </div>
+
             </div>
         )
     }
